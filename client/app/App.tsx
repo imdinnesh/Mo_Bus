01c0f56bf0eee22da3b854bd0f73891c85711164
imdinnesh <itsdinnesh@gmail.com>
2025-02-27 11:46:16 +0530

    feat(app): initialize React Native app structure and configuration

---
+++ client/app/App.tsx
@@ -0,0 +1,16 @@
+import { View,Text} from "react-native";
+function App(): React.JSX.Element {
+
+  
+
+  return (
+    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+      <Text>React Native</Text>
+      <Text>App</Text>
+    </View>
+
+    
+  );
+}
+
+export default App;
