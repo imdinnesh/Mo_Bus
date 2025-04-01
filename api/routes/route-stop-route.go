877bda4651a0615d108bffbaa82d7a6de194326f
imdinnesh <itsdinnesh@gmail.com>
2025-04-01 17:02:40 +0530

    route stop controller added

--- api/routes/route-stop-route.go
+++ api/routes/route-stop-route.go
@@ -1,9 +1,8 @@
 package routes
 
 import (
-	"github/imdinnes/mobusapi/database"
+	"github/imdinnes/mobusapi/controllers/routeStopController"
 	"github/imdinnes/mobusapi/middleware"
-	"net/http"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -11,76 +10,9 @@ import (
 func RouteStops(router *gin.RouterGroup, db *gorm.DB){
 	routeStopRouter:= router.Group("/route-stop",middleware.AuthMiddleware(),middleware.AdminMiddleware())
 
-	routeStopRouter.POST("add-route-stop",func(ctx *gin.Context) {
-		routeStop:=database.RouteStop{}
-		ctx.BindJSON(&routeStop)
-		if(routeStop.RouteID==0||routeStop.StopID==0||routeStop.StopIndex==0){
-			ctx.JSON(http.StatusBadRequest,gin.H{"error":"Route ID, Stop ID and Stop Index are required"})
-			return
-		}
+	routeStopRouter.POST("add-route-stop",routestopcontroller.AddRouteStop(db))
 
-		// check for valid route id
-		route:=database.Route{}
-		err:=db.Where("id=?",routeStop.RouteID).First(&route).Error
-		if err!=nil{
-			ctx.JSON(http.StatusBadRequest,gin.H{"error":"Invalid Route ID"})
-			return
-		}
-		// check for valid stop id
-		stop:=database.Stop{}
-		err=db.Where("id=?",routeStop.StopID).First(&stop).Error
-		if err!=nil{
-			ctx.JSON(http.StatusBadRequest,gin.H{"error":"Invalid Stop ID"})
-			return
-		}
-		err=db.Create(&routeStop).Error
-		if err!=nil{
-			ctx.JSON(http.StatusInternalServerError,gin.H{"error":"Failed to add route stop"})
-			return
-		}
-		ctx.JSON(http.StatusOK,gin.H{"message":"Route stop added successfully"})
+	routeStopRouter.PUT("/update-route-stop/:id",routestopcontroller.UpdateRouteStop(db))
 
-	})
-
-	routeStopRouter.PUT("/update-route-stop/:id",func(ctx *gin.Context) {
-		id:=ctx.Param("id")
-		routeStop:=database.RouteStop{}
-		ctx.BindJSON(&routeStop)
-		// here we can change the index only
-		if(routeStop.StopIndex==0){
-			ctx.JSON(http.StatusBadRequest,gin.H{"error":"Stop Index is required"})
-			return
-		}
-
-		dbRouteStop:=database.RouteStop{}
-		err:=db.Where("id=?",id).First(&dbRouteStop).Error
-		if err!=nil{
-			ctx.JSON(http.StatusBadRequest,gin.H{"error":"Invalid Route Stop ID"})
-			return
-		}
-
-		dbRouteStop.StopIndex=routeStop.StopIndex
-		err=db.Save(&dbRouteStop).Error
-		if err!=nil{
-			ctx.JSON(http.StatusInternalServerError,gin.H{"error":"Failed to update route stop"})
-			return
-		}
-		ctx.JSON(http.StatusOK,gin.H{"message":"Route stop updated successfully"})
-	})
-
-	routeStopRouter.DELETE("/delete-route-stop/:id",func(ctx *gin.Context) {
-		id:=ctx.Param("id")
-		routeStop:=database.RouteStop{}
-		err:=db.Where("id=?",id).First(&routeStop).Error
-		if err!=nil{
-			ctx.JSON(http.StatusBadRequest,gin.H{"error":"Invalid Route Stop ID"})
-			return
-		}
-		err=db.Delete(&routeStop).Error
-		if err!=nil{
-			ctx.JSON(http.StatusInternalServerError,gin.H{"error":"Failed to delete route stop"})
-			return
-		}
-		ctx.JSON(http.StatusOK,gin.H{"message":"Route stop deleted successfully"})
-	})
+	routeStopRouter.DELETE("/delete-route-stop/:id",routestopcontroller.DeleteRouteStop(db))
 }
