2fd57f34717dcc1621f20c691b29a4e3ddd6a775
imdinnesh <itsdinnesh@gmail.com>
2025-04-11 12:36:26 +0530

    accouncements with pagination route added

--- api/routes/announcements-route.go
+++ api/routes/announcements-route.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"gorm.io/gorm"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func AnnouncementRoutes(router *gin.RouterGroup,db *gorm.DB) {
@@ -31,6 +32,31 @@ func AnnouncementRoutes(router *gin.RouterGroup,db *gorm.DB) {
 
 	})
 
+	AccouncementRouter.GET("/",func(ctx *gin.Context) {
+		page:=ctx.Query("page")
+		var announcements []database.Accouncements
+		pageInt, err := strconv.Atoi(page)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid page number"})
+			return
+		}
+		if pageInt < 1 {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Page number must be greater than 0"})
+			return
+		}
+
+		if err := db.Offset((pageInt-1)*10).Limit(10).Find(&announcements).Error; err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch announcements"})
+			return
+		}
+		if len(announcements) == 0 {
+			ctx.JSON(http.StatusNotFound, gin.H{"message": "No announcements found"})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{"announcements": announcements})			
 	
+	})
+
+
 	
 }
