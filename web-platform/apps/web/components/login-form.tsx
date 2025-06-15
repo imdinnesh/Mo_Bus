ccfabcff62988169d61e270aa25a6c545fa0bb3e
imdinnesh <itsdinnesh@gmail.com>
2025-06-15 22:53:12 +0530

    add: shared repo

---
+++ web-platform/apps/web/components/login-form.tsx
@@ -0,0 +1,131 @@
+"use client"
+
+import { cn } from "@workspace/ui/lib/utils"
+import { Button } from "@workspace/ui/components/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card"
+import { Input } from "@workspace/ui/components/input"
+import { Label } from "@workspace/ui/components/label"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { loginSchema, LoginFormValues } from "@workspace/shared/schemas/auth"
+import { useMutation } from "@tanstack/react-query"
+import { login } from "@/api/auth"
+import { getDeviceId } from "@workspace/shared/lib"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const router = useRouter()
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormValues>({
+    resolver: zodResolver(loginSchema),
+  })
+
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: (data) => {
+      toast.success(data.message || "Login successful!")
+      localStorage.setItem("accessToken", data.access_token)
+      localStorage.setItem("refreshToken", data.refresh_token)
+      router.push("/dashboard")
+    },
+    onError: (error: any) => {
+      if (error.response.status===403){
+        toast.error("User not verified. Please verify your account.")
+        router.push(`/verify?email=${error.response.data.email}`)
+        return
+      }
+      toast.error(error.response?.data?.error || "Login failed. Please try again.")
+    },
+  })
+
+  const onSubmit = (data: LoginFormValues) => {
+    const device_id = getDeviceId()
+    mutation.mutate({ ...data, device_id })
+  }
+
+  const handleGoogleLogin = () => {
+    const device_id = getDeviceId()
+    window.location.href = `http://localhost:8080/api/v1/auth/google/login?device_id=${device_id}`
+  }
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="johndoe@example.com"
+                  {...register("email")}
+                  autoComplete="email"
+                />
+                {errors.email && (
+                  <p className="text-sm text-red-500">{errors.email.message}</p>
+                )}
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="••••••••"
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="text-sm text-red-500">{errors.password.message}</p>
+                )}
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full" disabled={mutation.isPending}>
+                  {mutation.isPending ? "Logging in..." : "Login"}
+                </Button>
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <a href="/signup" className="underline underline-offset-4">
+                Sign up
+              </a>
+            </div>
+          </form>
+          <Button 
+          onClick={handleGoogleLogin} variant="outline" className="w-full mt-4">
+            Login with Google
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
