0cfb191228bcaf94fb4b260989cf6819a1a5d5a6
imdinnesh <itsdinnesh@gmail.com>
2025-11-12 21:06:48 +0530

    add: resend otp service

--- mobile/hooks/auth.hooks.ts
+++ mobile/hooks/auth.hooks.ts
@@ -1,6 +1,7 @@
 import { useMutation } from "@tanstack/react-query";
-import { LoginService, SignupService } from "@/services/auth.service";
+import { LoginService, SendVerificationCodeService, SignupService } from "@/services/auth.service";
 import { useAuthStore } from "@/store/auth.store";
+import { toast } from "sonner-native";
 
 export const useLogin = () => {
     const setEmail = useAuthStore((state) => state.setEmail);
@@ -18,11 +19,24 @@ export const useLogin = () => {
             if (data?.refresh_token) setRefreshToken(data.refresh_token);
         },
 
-        onError: (error: any) => {
-            const status = error?.response?.status;
-            if(status===403){
+        onError: async (error: any, variables) => {
+            const status = error?.status;
 
+            // CASE: User not verified → send OTP automatically
+            if (status === 403 && variables?.email) {
+                try {
+                    const result = await SendVerificationCodeService(variables.email);
+                    setEmail(variables.email);
+                    toast.success("We sent you a verification code.");
+                    // Return a special flag to UI
+                    // throw { message: "USER_NOT_VERIFIED", redirectToVerify: true };
+                } catch (err:any) {
+                    toast.error(err.response.data.error||"Failed to send OTP. Try again.");
+                }
             }
+
+            // Normal error fallback
+            throw error;
         },
     });
 };
